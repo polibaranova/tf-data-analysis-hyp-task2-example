2fd10bea7709b38aa9ca8e2080da77ca83a5bae8
polibaranova <130179415+polibaranova@users.noreply.github.com>
2023-04-13 14:48:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 428014617 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    ks_stat, p_value = ks_2samp(x, y)
+    alpha = 0.06
+    critical_value = np.sqrt(-0.5 * np.log(alpha / 2)) / np.sqrt(len(x) + len(y))
+
+    if ks_stat > critical_value:
+        return True
+    else:
+        return False
